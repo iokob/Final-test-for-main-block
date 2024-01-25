4db380972b9a4357d902560709316d783288b856
ioko <ikobozev3@gmail.com>
2024-01-25 15:55:21 +0300

    program added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array = new string[5];
+Console.WriteLine("* * * Введите массив из 5 строк, разделяя каждый элемент enter * * *");
+void FillArrayFromConsole(string[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = Console.ReadLine();
+    }
+}
+
+void testPrintArray(string[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.WriteLine(array[i]);
+    }
+}
+
+FillArrayFromConsole(array);
+testPrintArray(array);
