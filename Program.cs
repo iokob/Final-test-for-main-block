1bcd2f7383b153a47e36495d76c8eee4e604c357
ioko <ikobozev3@gmail.com>
2024-01-26 15:11:52 +0300

    program finished

--- Program.cs
+++ Program.cs
@@ -6,11 +6,36 @@ void FillArrayFromConsole(string[] array) {
     }
 }
 
-void testPrintArray(string[] array) {
+void printArray(string[] array) {
     for (int i = 0; i < array.Length; i++) {
         Console.WriteLine(array[i]);
     }
 }
 
-FillArrayFromConsole(array);
-testPrintArray(array);
+string[] processingArray(string[] array) {
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) {
+        //считаем размер результирующего массива
+        if (array[i].Length <= 3) {
+            count++;
+        }
+    }
+    //создаём выходной массив, который будет результирующий
+    string[] rezArray = new string[count];
+    //обнуляем счётчик
+    count = 0;
+    //наполняем результирующий массив строками меньше или равными 3
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i].Length <= 3) {
+            rezArray[count] = array[i];
+            count++;
+        }
+    }
+    //осталось только возвратить результативный массив
+    return rezArray;
+}
+
+FillArrayFromConsole(array); //заполняем исходный массив
+string[] rezArray = processingArray(array); //результативный массив создаётся 
+//путём обработки исходного массива
+printArray(rezArray); //вывод конечного массива в консоль
